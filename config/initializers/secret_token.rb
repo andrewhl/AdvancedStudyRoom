eeae77a340efe63659de63b164092094a4371fa7
andrew <andrew@artgoboom.com>
2012-06-16 15:40:44 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvancedStudyRoom::Application.config.secret_token = '0b7c0a14221be2c8c96c276953ddfa1f76baee227716a4dc9bed3cb9144ba6766f5c57dd7949678c186d87169e3a534960637755cc8a693035121563ac8b9075'
