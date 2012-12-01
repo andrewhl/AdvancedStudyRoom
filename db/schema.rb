13040ba679d4878d727460b172159ed28f6185c0
andrew <ahlavats@rogers.com>
2012-12-01 17:37:56 -0500

    merging

--- db/schema.rb
+++ db/schema.rb
@@ -128,35 +128,12 @@ ActiveRecord::Schema.define(:version => 20121201214419) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.string   "ruleset_name"
-    t.boolean  "allowed_rengo"
-    t.boolean  "allowed_teaching"
-    t.boolean  "allowed_review"
-    t.boolean  "allowed_free"
-    t.boolean  "allowed_rated"
-    t.boolean  "allowed_simul"
-    t.boolean  "allowed_demonstration"
-    t.string   "tag_text"
-    t.float    "main_time_min"
-    t.float    "main_time_max"
-    t.boolean  "overtime_required"
-    t.boolean  "jovertime_allowed"
-    t.boolean  "covertime_allowed"
-    t.integer  "jot_min_periods"
-    t.integer  "jot_max_periods"
-    t.float    "jot_min_period_length"
-    t.float    "jot_max_period_length"
-    t.integer  "cot_min_stones"
-    t.integer  "cot_max_stones"
-    t.float    "cot_max_time"
-    t.float    "cot_min_time"
-    t.float    "handicap_default"
+    t.string   "event_type"
     t.integer  "ruleset_default"
-    t.integer  "games_per_player"
-    t.integer  "games_per_opponent"
     t.integer  "league_id"
     t.integer  "server_id"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   add_index "events", ["league_id"], :name => "index_events_on_league_id"
