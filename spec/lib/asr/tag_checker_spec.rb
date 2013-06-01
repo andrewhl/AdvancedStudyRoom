d209b1f427eabda2e3962be16a66f0c8c06055bb
andrew <andrew@artgoboom.com>
2013-06-01 11:06:00 -0400

    adding a couple more tests for node limit (closes #22)

--- spec/lib/asr/tag_checker_spec.rb
+++ spec/lib/asr/tag_checker_spec.rb
@@ -7,10 +7,19 @@ describe ASR::TagChecker do
     ASR::TagChecker.new(event_tags)
   }
 
-
   it "should fail if the node limit is exceeded" do
     match_tags = [FactoryGirl.build(:match_tag, phrase: '#asr', node: 15)]
     tag_checker.tagged?(match_tags, 10).should be_false
+  end
+
+  it "should pass if the node limit is met" do
+    match_tags = [FactoryGirl.build(:match_tag, phrase: '#asr', node: 10)]
+    tag_checker.tagged?(match_tags, 10).should be_true
+  end
+
+  it "should pass if the node limit is higher" do
+    match_tags = [FactoryGirl.build(:match_tag, phrase: '#asr', node: 5)]
+    tag_checker.tagged?(match_tags, 10).should be_true
   end
 
 end
