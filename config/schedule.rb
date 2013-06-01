cc0eee64f47917d35fd8e6b532681bbb3f88a4d5
andrew <andrew@artgoboom.com>
2013-06-01 02:44:03 -0400

    update to cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,12 @@
 every 6.hours do
     rake "db:backup", output: 'log/db-backup.log'
 end
+
+every 4.hours do
+    rake "manager:import"
+    rake "manager:validate"
+    rake "manager:tags"
+    rake "manager:points"
+end
+
+
