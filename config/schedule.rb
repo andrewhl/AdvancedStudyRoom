f41c32e9396bb01e7ccbd5797dd255b101c6fc07
andrew <andrew@artgoboom.com>
2013-06-02 02:20:22 -0400

    adding point tally to cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,9 +4,10 @@ every 6.hours do
     rake 'db:backup', output: 'log/db-backup.log'
 end
 
-every 4.hours do
+every :hour do
     rake 'manager:import'
     rake 'manager:validate'
     rake 'manager:tags'
     rake 'manager:points'
+    rake 'manager:points:total'
 end
