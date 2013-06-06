5d26ed911f8f065fb546e353f95369dae3dfdd60
Arturo Guzman <arturo@kisurfer.com>
2013-06-06 03:33:01 -0400

    remove unused user views update sidebar admin user link style total played games progress bar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,22 +14,12 @@ class EventsController < ApplicationController
     @leagues = Event.leagues
   end
 
-  def join_other
-    @event = Event.find(params[:id])
-    account = Account.find(params[:account_id])
-
-    registration = account.registrations.find_or_create_by_event_id(@event.id)
-    registration.update_attributes({active: true}, without_protection: true)
-    redirect_to registration.account.user,
-      flash: {success: "You have registered #{registration.account.handle} to #{@event.name}"}
-  end
-
   def join
     @event = Event.find(params[:id])
     account = current_user.accounts.where(server_id: @event.server_id).first
     if account
       registration = account.registrations.find_or_create_by_event_id(@event.id)
-      registration.update_attributes({active: true}, without_protection: true)
+      registration.update_attributes({active: true, division_id: nil}, without_protection: true)
       redirect_to profile_path,
         flash: {success: "You have joined #{@event.name}, you will be assigned to a division soon"}
     else
@@ -40,15 +30,11 @@ class EventsController < ApplicationController
 
   def quit
     reg = current_user.registrations.find(params[:registration_id])
-    if reg && !reg.update_attributes({active: false, division_id: nil}, without_protection: true)
+    if reg && !reg.update_attributes({active: false}, without_protection: true)
       flash[:error] = 'There was an error while deleting your registration'
     end
 
-    if current_user.admin?
-      redirect_to reg.account.user, flash: {success: "This user has been removed from this event."}
-    else
-      redirect_to profile_path, flash: {success: "You have been removed from this event."}
-    end
+    redirect_to profile_path, flash: {success: "You have been removed from this event."}
   end
 
 end
