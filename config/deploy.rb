7ecb52605000fbc2b3256e0d044b4b65ff580582
Arturo Guzman <arturo@kisurfer.com>
2013-06-09 14:02:42 -0400

    add staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,14 @@
-set :application, 'AdvancedStudyRoom'
-set :domain,      'web-app@beta.advancedstudyroom.com'
-set :deploy_to,   '/home/web-app/apps/advanced_study_room'
-set :repository,  'https://github.com/andrewhl/AdvancedStudyRoom'
-set :revision,    'origin/master'
-set :bundle_cmd,  'bundle'
+set :application,       'AdvancedStudyRoom'
+set :domain,            'web-app@test.advancedstudyroom.com'
+set :deploy_to,         '/home/web-app/apps/advanced_study_room'
+set :repository,        'https://github.com/andrewhl/AdvancedStudyRoom'
+set :revision,          'origin/develop'
+set :bundle_cmd,        'bundle'
+
+task :production do
+  set :revision,        'origin/master'
+  set :domain,          'web-app@beta.advancedstudyroom.com'
+end
 
 set :shared_paths, {
   'assets'              => 'public/assets',
