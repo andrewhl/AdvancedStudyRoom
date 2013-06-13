4188a597ee7496ce67f07e2f7fdd2827c64fa194
andrew <andrew@artgoboom.com>
2013-06-12 22:41:42 -0400

    migrating event types

---
+++ db/migrate/20130613021357_change_event_type_to_event_type_id.rb
@@ -0,0 +1,28 @@
+class ChangeEventTypeToEventTypeId < ActiveRecord::Migration
+
+  def up
+    add_column :events, :event_type_id, :integer
+    event_type_names = Event.all.collect { |event| event.read_attribute(:event_type).to_s.downcase }.uniq
+    event_type_names.each do |event_type_name|
+      event_type = EventType.find_or_create_by_name(event_type_name)
+      events = Event.where("LOWER(event_type) = LOWER(?)", event_type.name)
+      events.each do |event|
+        event.update_attribute(:event_type_id, event.id)
+      end
+    end
+    remove_column :events, :event_type
+    add_index :events, :event_type_id
+  end
+
+  def down
+    remove_index :events, :event_type_id
+    add_column :events, :event_type, :string
+    events = Event.all
+    events.each do |event|
+      event_type = EventType.find(event.event_type_id).name
+      event.update_attribute(:event_type, event_type)
+    end
+
+    remove_column :events, :event_type_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613020802) do
+ActiveRecord::Schema.define(:version => 20130613021357) do
 
   create_table "accounts", :force => true do |t|
     t.string   "handle"
@@ -73,14 +73,15 @@ ActiveRecord::Schema.define(:version => 20130613020802) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
-    t.string   "event_type"
     t.integer  "server_id"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.text     "description"
     t.text     "prizes_description"
+    t.integer  "event_type_id"
   end
 
+  add_index "events", ["event_type_id"], :name => "index_events_on_event_type_id"
   add_index "events", ["server_id"], :name => "index_events_on_server_id"
 
   create_table "match_tags", :force => true do |t|
