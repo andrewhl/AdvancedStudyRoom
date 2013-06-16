817a65d9365216d53bcd5dc289597eeeb38adaca
andrew <andrew@artgoboom.com>
2013-06-15 20:07:50 -0400

    migrations

--- db/migrate/20130613021357_change_event_type_to_event_type_id.rb
+++ db/migrate/20130613021357_change_event_type_to_event_type_id.rb
@@ -20,6 +20,7 @@ class ChangeEventTypeToEventTypeId < ActiveRecord::Migration
     events = Event.all
     events.each do |event|
       event_type_name = EventType.find(event.event_type_id).name
+      event.update_attribute :event_type, event_type_name
       ActiveRecord::Base.connection.execute("UPDATE events SET event_type = '#{event_type_name}' WHERE id = #{event.id}")
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613035927) do
+ActiveRecord::Schema.define(:version => 20130613042939) do
 
   create_table "accounts", :force => true do |t|
     t.string   "handle"
